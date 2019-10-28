9dffbfd63f42fc75af08a10d74c7aa93e3bcb6d4
Shiraga <matheus.s.rasera@gmail.com>
2019-10-27 22:37:40 -0300

    Tentativa frustrada de implementação do sistema de login

--- ionic/PlayDietas/src/app/app-routing.module.ts
+++ ionic/PlayDietas/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
   {
@@ -7,20 +8,42 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  },
-  {
-    path: 'list',
-    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  }
+  { path: 'landing', loadChildren: './pages/landing/landing.module#LandingPageModule' },  
+  { path: 'login', loadChildren: './pages/auth/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './pages/auth/register/register.module#RegisterPageModule' },
+  
+  { path: 'dashboard', loadChildren: './pages/dashboard/dashboard.module#DashboardPageModule', canActivate: [AuthGuard] },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule'},
+  { path: 'list', loadChildren: './list/list.module#ListPageModule'},
 ];
-
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule {}
+
+// import { NgModule } from '@angular/core';
+// import { PreloadAllModules, Routes, RouterModule } from '@angular/router';
+// import { AuthGuard } from './guard/auth.guard';
+// const routes: Routes = [
+//   {
+//     path: '',
+//     redirectTo: 'landing',
+//     pathMatch: 'full'
+//   },
+//   { path: 'landing', loadChildren: './pages/landing/landing.module#LandingPageModule' },  
+//   { path: 'login', loadChildren: './pages/auth/login/login.module#LoginPageModule' },
+//   { path: 'register', loadChildren: './pages/auth/register/register.module#RegisterPageModule' },
+  
+//   { path: 'dashboard', loadChildren: './pages/dashboard/dashboard.module#DashboardPageModule', canActivate: [AuthGuard] },
+//   { path: 'home', loadChildren: './home/home.module#HomePageModule', canActivate: [AuthGuard] },
+//   { path: 'list', loadChildren: './list/list.module#ListPageModule', canActivate: [AuthGuard] },
+
+// @NgModule({
+//   imports: [
+//     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+//   ],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule {}
