d13bd504818a54ac12a2bc6ccded0a5844e6a528
Shiraga <matheus.s.rasera@gmail.com>
2019-12-02 23:09:35 -0300

    CSS do dashboard

--- ionic/NewDietas/src/app/app-routing.module.ts
+++ ionic/NewDietas/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AuthGuard } from './guard/auth.guard';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'landing',
+    redirectTo: 'dashboard',
     pathMatch: 'full'
   },
   { path: 'landing', loadChildren: './pages/landing/landing.module#LandingPageModule' },  
